0bf9c88fda5cf6fe8223bf08863fafea21956527
Jen B <jenbutler984@gmail.com>
2023-07-22 06:48:18 -0400

    Added virtuals

--- models/User.js
+++ models/User.js
@@ -27,6 +27,14 @@ const UserSchema = new Schema({
   ]
 });
 
+UserSchema.virtual('thoughtCount').get(function () {
+  return this.thoughts.length;
+})
+
+UserSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+})
+
 const User = model('User', UserSchema);
 
 module.exports = User;
